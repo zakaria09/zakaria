32b04357c0a7bfb384f3a72ff59aa60e1fce6dab
zakaria arr <zakaria.09@hotmail.co.uk>
2024-02-29 18:01:15 +0000

    migrated home page from buttercms to sanity

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -1,8 +1,8 @@
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
-import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
 import { codeInput } from '@sanity/code-input'
+import { visionTool } from '@sanity/vision'
 
 export default defineConfig({
   name: 'default',
