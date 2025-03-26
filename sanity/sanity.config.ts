b83eaae73aa3db8a6be0b907c07e7bf62e0320a3
zakaria arr <zakaria.09@hotmail.co.uk>
2025-03-26 16:07:35 +0000

    fix

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -1,7 +1,6 @@
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
-import {codeInput} from '@sanity/code-input'
 import {schemaTypes} from './schemaTypes'
 
 export default defineConfig({
@@ -12,7 +11,7 @@ export default defineConfig({
   dataset: 'production',
   basePath: '/studio',
 
-  plugins: [structureTool(), visionTool(), codeInput()],
+  plugins: [structureTool(), visionTool()],
 
   schema: {
     types: schemaTypes,
